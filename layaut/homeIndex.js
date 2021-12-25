d8d5e604266509a53d25ee13d831d5d8f11e6dc4
Jorge Duron <duronj08@gmail.com>
2021-12-24 18:57:58 -0600

    prueba de grid

--- layaut/homeIndex.js
+++ layaut/homeIndex.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import Head from "next/head";
 const homeIndex = () => {
-  return <>{props.children}</>;
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      {props.children}
+    </>
+  );
 };
 
 export default homeIndex;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
-import Head from "next/head";
 import Card from "../components/Card";
 import { useState, useEffect } from "react";
 import { getDataImage } from "../services/apiconfig/indexApi";
-
+import homeIndex from "../layaut/homeIndex";
+import styled from "styled-components";
+const GridImages = styled.main`
+  column-gap: 10px;
+  -webkit-column-gap: 10px;
+  -moz-column-gap: 10px;
+  -webkit-column-width: 400px;
+  -moz-column-width: 400px;
+  column-width: 400px;
+  counter-reset: item;
+  list-style: none;
+  margin: 0 auto;
+  padding: 20px 10px;
+  width: 100%;
+  position: relative;
+  box-sizing: border-box;
+`;
 export default function Home() {
   const [data, setData] = useState([]);
   const getTopImage = async (pageId) => {
@@ -22,11 +37,21 @@ export default function Home() {
     getTopImage(1);
   }, []);
   return (
-    <div>
-      {data.map(({ id, urls, alt_description, width, height }) => {
-        const { full } = urls;
-        return <Card key={id} image={full} title={alt_description} w={width} h={height} />;
-      })}
-    </div>
+    <homeIndex>
+      <GridImages>
+        {data.map(({ id, urls, alt_description, width, height }) => {
+          const { full } = urls;
+          return (
+            <Card
+              key={id}
+              image={full}
+              title={alt_description}
+              w={width}
+              h={height}
+            />
+          );
+        })}
+      </GridImages>
+    </homeIndex>
   );
 }
