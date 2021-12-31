2096a2e59b81939b47e980b73da120253f7d72c4
Jorge Durón <48816029+JorgeDevJ@users.noreply.github.com>
2021-12-31 00:56:45 -0600

    opt opt

--- pages/api/searchimage.js
+++ pages/api/searchimage.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const Search = async (req, res) => {
+const Search = async (req, resresresres) => {
   const {
     query: { page, query },
   } = req;
-  const URL = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&per_page=30`;
+  const URL = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&per_page=15`;
   const response = await axios.get(URL, {
     params: {
       client_id: process.env.KEY_UNSPLASH,
